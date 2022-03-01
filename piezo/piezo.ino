bac77b11f449e0c8ecdce9ef589b97452fd13eb6
ingenuitive <solak.chu@gmail.com>
2022-02-28 23:36:32 -0500

    week 5

---
+++ piezo/piezo.ino
@@ -0,0 +1,31 @@
+const int sensor = A0;     
+const int A1A = 3;
+const int A1B = 4;
+
+int sensorReading = 0;         
+
+void setup() {
+  pinMode(A1A, OUTPUT);
+  pinMode(A1B, OUTPUT);
+  digitalWrite(A1A, LOW);
+  digitalWrite(A1B, LOW);
+    Serial.begin(9600);
+
+}
+
+void loop() {
+  sensorReading = analogRead(sensor);
+  
+  if (sensorReading >= 25) {
+    digitalWrite(A1A, HIGH);   
+    digitalWrite(A1B, LOW);
+    Serial.println(sensorReading);
+    delay(3000);
+    digitalWrite(A1A, LOW);
+    delay(3000);
+    digitalWrite(A1A, LOW);   
+    digitalWrite(A1B, HIGH);
+    delay(3000);
+    digitalWrite(A1B, LOW);
+  } 
+}
